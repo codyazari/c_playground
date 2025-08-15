1de695c59b9a2df120e4aecabf783be8f585539a
codyazari <azaricody@gmail.com>
2025-08-15 10:42:14 -0400

    Removing executables

--- data_structures/graphs/sandbox/main.c
+++ data_structures/graphs/sandbox/main.c
@@ -18,19 +18,35 @@ typedef struct Room {
 typedef struct Graph {
     int n_rooms;
     bool is_directed;
-    Room* neighbors[];
+    Room* neighbors;
 } Graph;
 
-Graph* generate_graph(size_t n_rooms, bool is_directed) {
+Graph* generate_graph(size_t n_initial_rooms, bool is_directed) {
     Graph* g = malloc(sizeof *g);
-    if (!g) perror("Failed to allocate memory for graph"); exit(EXIT_FAILURE);
+    if (!g) {
+        perror("Failed to allocate memory for graph");
+        exit(EXIT_FAILURE);
+    }
 
-    g->n_rooms = n_rooms;
-    g->is_directed = false;
-    g->adj = calloc(n_rooms, )
+    g->n_rooms = n_initial_rooms;
+    g->is_directed = is_directed;
+    g->neighbors = malloc(n_initial_rooms * sizeof *g->neighbors);
+    if (!g->neighbors) {
+        perror("Failed to allocate memory for n");
+        exit(EXIT_FAILURE);
+    }
+    for (size_t i = 0; i < n_initial_rooms; i++) {
+        g->neighbors[i].head = NULL;
+        g->neighbors[i].name = NULL;
+        g->neighbors[i].x = 0;
+        g->neighbors[i].y = 0;
+    }
+
+    return g;
 }
 
 int main(void) {
-    printf("Hello\n");
+    Graph* g = generate_graph(10, false);
+    printf("%d\n", g->neighbors[1].x);
     return 0;
 }
