6d3790a0d9c43781df803cdb9776012ba227424d
codyazari <azaricody@gmail.com>
2025-08-16 09:38:25 -0400

    Redoing the graph again - this time a bit more from memory. I actually think I'll keep this rev

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # c_playground
 My personal sandbox for all C work
+
+This is mostly just to keep track of the work I do. None of these are production ready obviously - just the crazed meanderings of a sub-par coder exploring C. 
+
+If you do want to run these (Linux only) - use the following gcc command: 
+
+`gcc main.c -Wall -Wextra -std=c2x --pedantic && ./a.out`
--- data_structures/graphs/sandbox/main.c
+++ data_structures/graphs/sandbox/main.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
+// EDGE
 typedef struct Hallway {
-    int to_id;
-    int from_id;
+    int room_id;
+    int weight;
     struct Hallway* next;
 } Hallway;
 
+// Node or Vertices
 typedef struct Room {
+    int room_id;
+    int n_neighbors;
     Hallway* head;
-    char* name;
-    int x, y;
 } Room;
 
-// 
 typedef struct Graph {
-    int n_rooms;
+    size_t n_rooms;
+    size_t n_hallways;
     bool is_directed;
-    Room* neighbors;
+    Room* adj_rooms;
 } Graph;
 
-Graph* generate_graph(size_t n_initial_rooms, bool is_directed) {
+Graph* generate_graph(size_t n_rooms, bool is_directed) {
     Graph* g = malloc(sizeof *g);
     if (!g) {
-        perror("Failed to allocate memory for graph");
+        fprintf(stderr, "Could not allocate memory for graph");
         exit(EXIT_FAILURE);
     }
-
-    g->n_rooms = n_initial_rooms;
+    g->n_rooms = n_rooms;
+    g->n_hallways = 0;
     g->is_directed = is_directed;
-    g->neighbors = malloc(n_initial_rooms * sizeof *g->neighbors);
-    if (!g->neighbors) {
-        perror("Failed to allocate memory for n");
+    g->adj_rooms = malloc(n_rooms * sizeof *g->adj_rooms);
+    if (!g->adj_rooms) {
+        fprintf(stderr, "Could not allocate memory for Room Adjacency list");
         exit(EXIT_FAILURE);
     }
-    for (size_t i = 0; i < n_initial_rooms; i++) {
-        g->neighbors[i].head = NULL;
-        g->neighbors[i].name = NULL;
-        g->neighbors[i].x = 0;
-        g->neighbors[i].y = 0;
-    }
 
+    for (size_t i = 0; i < g->n_rooms; i++) {
+        g->adj_rooms[i].head = NULL;
+        g->adj_rooms[i].room_id = i;
+        g->adj_rooms[i].n_neighbors = 0;
+    }
     return g;
 }
 
+// ADD EDGE -- CONNECT TWO NODES
+void add_hallway(Graph* g, int x, int y) {
+
+}
+
 int main(void) {
-    Graph* g = generate_graph(10, false);
-    printf("%d\n", g->neighbors[1].x);
+    Graph* g = generate_graph(100, false);
+    printf("%d\n", g->adj_rooms[99].room_id);
     return 0;
 }
